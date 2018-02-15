98a7c849bacd86063cddcbecd087c70fb66a7e4b
Kevin Huang <yellow.khaos@gmail.com>
2018-02-15 22:32:12 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,5 +9,13 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration
-  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.decimal :price
+      t.string :image_url
+      t.integer :size
+      t.timestamps
+    end
+  end
 end
